a490227992d7ae33c648581f60c9d78ec5353123
abiney1321 <abiney1321@gmail.com>
2025-02-18 21:18:30 +0000

    error handling

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,19 +1,31 @@
 import { useEffect, useState } from "react";
 import FeedBackItem from "./FeedBackItem";
 import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
     fetch(
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
     )
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error();
+        }
+
+        res.json();
+      })
       .then((data) => {
         setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setErrorMessage("Something went wrong");
         setIsLoading(false);
       });
   }, []);
@@ -21,6 +33,8 @@ export default function FeedbackList() {
   return (
     <ol className="feedback-list">
       {isLoading ? <Spinner /> : null}
+
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
 
       {feedbackItems.map((feedbackItem) => (
         <FeedBackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
