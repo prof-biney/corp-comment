89d72551e688487743e9a74e12d55851d1faba64
abiney1321 <abiney1321@gmail.com>
2025-02-20 11:02:43 +0000

    form validation

--- src/components/feedback/FeedbackForm.tsx
+++ src/components/feedback/FeedbackForm.tsx
@@ -7,6 +7,8 @@ type FeedBackFormProps = {
 
 export default function FeedbackForm({ onAddToList }: FeedBackFormProps) {
   const [text, setText] = useState("");
+  const [showValidIndicator, setShowValidIndicator] = useState(false);
+  const [showInvalidIndicator, setShowInvalidIndicator] = useState(false);
   const charCount = MAX_CHARACTERS - text.length;
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -17,12 +19,28 @@ export default function FeedbackForm({ onAddToList }: FeedBackFormProps) {
 
   const handleSubmit = function (e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    // basic validation
+    if (text.includes("#") && text.length >= 5) {
+      setShowValidIndicator(true);
+      setTimeout(() => setShowValidIndicator(false), 2000);
+    } else {
+      setShowInvalidIndicator(true);
+      setTimeout(() => setShowInvalidIndicator(false), 2000);
+      return;
+    }
+
     onAddToList(text);
     setText("");
   };
 
   return (
-    <form onSubmit={handleSubmit} className="form">
+    <form
+      onSubmit={handleSubmit}
+      className={`form ${showValidIndicator ? "form--valid" : ""} ${
+        showInvalidIndicator ? "form--invalid" : ""
+      }`}
+    >
       <textarea
         onChange={handleChange}
         value={text}
